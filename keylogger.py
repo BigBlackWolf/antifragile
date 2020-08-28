10fa411e5f4d30ea3561d61e9a94496f60710c2d
BigBlackWolf <stepan.rasputny@gmail.com>
2020-08-28 18:00:45 +0300

    Added keylogger for tracking self typing

---
+++ keylogger.py
@@ -0,0 +1,25 @@
+import keyboard
+import datetime
+
+HOME_DIR = "/home/buch/keylogger_storage.txt"
+STORAGE = []
+
+
+def print_pressed(e):
+    if e.event_type == "down":
+        STORAGE.append(e.name + ",")
+    if len(STORAGE) > 200:
+        with open(HOME_DIR, "a") as file:
+            file.writelines(STORAGE)
+            STORAGE.clear()
+
+
+def main():
+    date = datetime.datetime.now()
+    STORAGE.append("\n" + str(date) + " ")
+    keyboard.hook(print_pressed)
+    keyboard.wait()
+
+
+if __name__ == '__main__':
+    main()
